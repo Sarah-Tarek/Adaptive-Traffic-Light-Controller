9fcc182656059b16e5a8a0b5f423f64cb1365971
Mahmoud Abdelraouf <mahmoud126125@gmail.com>
2024-12-06 18:11:30 +0200

    add comments

--- src/traffic_light_fsm.v
+++ src/traffic_light_fsm.v
@@ -1,26 +1,26 @@
 module traffic_light_fsm (
-    input wire clk,                 // System clock
-    input wire rst,                 // Reset signal
-    input wire [3:0] S1,            // Start sensors for all 4 lanes
-    input wire [3:0] S5,            // Congestion sensors for all 4 lanes
-    output reg [3:0] state,         // FSM state
-    output reg [3:0] next_state,    // Next FSM state
-    output reg [3:0] light_signal   // Traffic light control signal
+    input wire clk,                  // System clock
+    input wire rst,                  // Reset signal
+    input wire [3:0] S1,             // Start sensors for all 4 lanes
+    input wire [3:0] S5,             // Congestion sensors for all 4 lanes
+    output reg [3:0] state,          // FSM state
+    output reg [3:0] next_state,     // Next FSM state
+    output reg [3:0] light_signal    // Traffic light control signal
 );
 
     // State encoding
-    localparam NS1_RED    = 4'b0000,  
-               NS1_GREEN  = 4'b0001,  
-               NS1_YELLOW = 4'b0011,  
-               NS2_RED    = 4'b0010,  
-               NS2_GREEN  = 4'b0110,  
-               NS2_YELLOW = 4'b0111,  
-               EW1_RED    = 4'b0101,  
-               EW1_GREEN  = 4'b0100,  
-               EW1_YELLOW = 4'b1100,
-               EW2_RED    = 4'b1101,
-               EW2_GREEN  = 4'b1111,
-               EW2_YELLOW = 4'b1110;
+    localparam NS1_RED    = 4'b0000, // Gray Code: 0
+               NS1_GREEN  = 4'b0001, // Gray Code: 1
+               NS1_YELLOW = 4'b0011, // Gray Code: 3
+               NS2_RED    = 4'b0010, // Gray Code: 2
+               NS2_GREEN  = 4'b0110, // Gray Code: 6
+               NS2_YELLOW = 4'b0111, // Gray Code: 7
+               EW1_RED    = 4'b0101, // Gray Code: 5
+               EW1_GREEN  = 4'b0100, // Gray Code: 4
+               EW1_YELLOW = 4'b1100, // Gray Code: 12
+               EW2_RED    = 4'b1101, // Gray Code: 13
+               EW2_GREEN  = 4'b1111, // Gray Code: 15
+               EW2_YELLOW = 4'b1110; // Gray Code: 14
 
     // State transition logic
     always @(posedge clk or posedge rst) begin
@@ -53,7 +53,7 @@ module traffic_light_fsm (
             default:    next_state = NS1_RED;  // Safe default state
         endcase
     end
-    
+
     // FSM Output Logic (Based on Current State)
     always @(*) begin
         case (state)
