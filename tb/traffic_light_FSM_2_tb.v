6ca1104ef48cc5e04a1b6dfd6460a14997eb19fe
MariamAmrE <130848939+MariamAmrE@users.noreply.github.com>
2024-12-08 12:10:51 +0200

    Update traffic_light_FSM_2_tb.v

--- tb/traffic_light_FSM_2_tb.v
+++ tb/traffic_light_FSM_2_tb.v
@@ -10,11 +10,10 @@ module FSM_tb();
   reg EW_S5_tb;
   reg WE_S5_tb;
   wire [3:0] state_tb;
-  wire [3:0] next_state_tb;
   wire [3:0] light_signal_tb;
   
 //instantiate DUT  
-  traffic_light_fsm DUT(.clk(clk_tb) , .rst(rst_tb) , .NS_S1(NS_S1_tb) , .SN_S1(SN_S1_tb) ,.EW_S1(EW_S1_tb) ,.WE_S1(WE_S1_tb) , .NS_S5(NS_S5_tb) , .SN_S5(SN_S5_tb) ,.EW_S5(EW_S5_tb) ,.WE_S5(WE_S5_tb) , .state(state_tb) , .next_state(next_state_tb) , .light_signal(light_signal_tb));
+  fsm DUT(.clk(clk_tb) , .rst(rst_tb) , .NS_S1(NS_S1_tb) , .SN_S1(SN_S1_tb) ,.EW_S1(EW_S1_tb) ,.WE_S1(WE_S1_tb) , .NS_S5(NS_S5_tb) , .SN_S5(SN_S5_tb) ,.EW_S5(EW_S5_tb) ,.WE_S5(WE_S5_tb) , .state(state_tb), .light_signal(light_signal_tb));
   
   //clk generator
   initial begin
@@ -30,9 +29,8 @@ module FSM_tb();
     #1 rst_tb <= 1'b0;
   end
   
-  //test cases: testing every transition
+  //test cases: testing every transition and reset
   initial begin
-  // NS_RED --> SN_RED --> EW_RED --> WE_RED --> NS_RED
   NS_S1_tb <= 1'b0;
   SN_S1_tb <= 1'b0;
   EW_S1_tb <= 1'b0;
@@ -43,60 +41,47 @@ module FSM_tb();
   EW_S5_tb <= 1'b0;
   WE_S5_tb <= 1'b0;  
   #42
-  //NS  
-  //NS_RED --> NS_PRIMARY_GREEN  
   NS_S1_tb <= 1'b1;
   #8
-  //NS_PRIMARY_GREEN --> NS_EXTENDED_GREEN
   NS_S5_tb <= 1'b1;
   #10
-  //NS_EXTENDED_GREEN --> NS_YELLOW
-  #10
-  //NS_YELLOW --> SN_RED
-  #10
-  //SN 
-  //SN_RED --> SN_PRIMARY_GREEN
   SN_S1_tb <= 1'b1; 
   #10
-  //SN_PRIMARY_GREEN --> SN_EXTENDED_GREEN
   SN_S5_tb <= 1'b1;
-  #10
-  //SN_EXTENDED_GREEN --> SN_YELLOW
-  #10  
-  //SN_YELLOW --> EW_RED
-  #10
-  //EW
-  //EW_RED --> EW_PRIMARY_GREEN
+  #10 
   EW_S1_tb <= 1'b1; 
   #10
-  //EW_PRIMARY_GREEN --> EW_EXTENDED_GREEN
   EW_S5_tb <= 1'b1;
   #10
-  //EW_EXTENDED_GREEN --> EW_YELLOW
   #10  
-  //EW_YELLOW --> WE_RED
   #10
-   //WE
-  //WE_RED --> WE_PRIMARY_GREEN
   WE_S1_tb <= 1'b1; 
   #10
-  //WE_PRIMARY_GREEN --> WE_EXTENDED_GREEN
   WE_S5_tb <= 1'b1;
   #10
-  //WE_EXTENDED_GREEN --> WE_YELLOW
   #10  
-  //WE_YELLOW --> NS_RED
   #20
   rst_tb <= 1'b1;
   #12
   rst_tb <= 1'b0;
-  #30      
+  #20
+  NS_S5_tb <= 1'b0;
+  #10
+  SN_S5_tb <= 1'b0;
+  #10
+  EW_S5_tb <= 1'b0;
+  #10
+  WE_S5_tb <= 1'b0;
+  #10
+  SN_S1_tb <= 1'b0;  
+  #10  
+    
   $finish ;    
   end
   
   //
   always@(posedge clk_tb)begin
-    $display("state = %0b , next state = %0b , Light signal = %0b" , state_tb , next_state_tb , light_signal_tb);
+    $display("state = %0b , next state = %0b , Light signal = %0b" , state_tb , DUT.next_state , light_signal_tb);
   end 
-  
+ 
   endmodule
